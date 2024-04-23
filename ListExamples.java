8c81a8c072006fcc6760d6530b0d80ed610bedf9
SamH314 <samuelhernandez010904@gmail.com>
2024-04-23 11:09:56 -0700

    fixed bugs in ListExamples

--- ListExamples.java
+++ ListExamples.java
@@ -3,16 +3,43 @@ import java.util.List;
 
 interface StringChecker { boolean checkString(String s); }
 
+class ChooseShortWord implements StringChecker{
+  @Override
+  public boolean checkString(String s){
+    return s.length() < 5;
+  }
+}
+
+class DogChecker implements StringChecker {
+  @Override
+  public boolean checkString(String s) {
+      return s.startsWith("dog");
+  }
+}
+
 class ListExamples {
 
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
+  // Bug: list filter reverses the list rather than keeping them in same order
   static List<String> filter(List<String> list, StringChecker sc) {
     List<String> result = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
+        // problem line, adds it to front of list instead of rear
         result.add(0, s);
+      }
+    }
+    return result;
+  }
+
+  //fixed
+  static List<String> filter2(List<String> list, StringChecker sc) {
+    List<String> result = new ArrayList<>();
+    for(String s: list) {
+      if(sc.checkString(s)) {
+        result.add(s);
       }
     }
     return result;
@@ -21,6 +48,7 @@ class ListExamples {
 
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
+  // Bug: list merge – look at the last few indices very closely!
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
@@ -40,7 +68,34 @@ class ListExamples {
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
+      // problem line; infinite loop. keeps adding the same 
+      // element bc it increments index1 instead of 2
       index1 += 1;
+    }
+    return result;
+  }
+
+  //fixed
+  static List<String> merge2(List<String> list1, List<String> list2) {
+    List<String> result = new ArrayList<>();
+    int index1 = 0, index2 = 0;
+    while(index1 < list1.size() && index2 < list2.size()) {
+      if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
+        result.add(list1.get(index1));
+        index1 += 1;
+      }
+      else {
+        result.add(list2.get(index2));
+        index2 += 1;
+      }
+    }
+    while(index1 < list1.size()) {
+      result.add(list1.get(index1));
+      index1 += 1;
+    }
+    while(index2 < list2.size()) {
+      result.add(list2.get(index2));
+      index2 += 1;
     }
     return result;
   }
